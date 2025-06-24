fb61d7c676a02d6c415ab98943d863837e9bf6dc
Jonas Werner <106274138+Jonas-wastaken@users.noreply.github.com>
2025-06-24 13:13:40 +0200

    frontend template

---
+++ demo/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Demo",
+    page_icon=None,
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items=None,
+)
+
+st.title("Hello World")
